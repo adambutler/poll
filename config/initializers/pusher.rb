4e01230eae75c0e430f4358cd3a504dd4006390a
Adam Butler <adam@littlebigmedia.co.uk>
2014-10-13 12:12:25 +0100

    Setup host for development

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,6 +1,7 @@
 require 'pusher'
 
-
-
 Pusher.url = "http://#{CONFIG[:pusher]["key"]}:#{CONFIG[:pusher]["secret"]}@api.pusherapp.com/apps/91795"
 Pusher.logger = Rails.logger
+
+Pusher.host   = 'localhost'
+Pusher.port   = 4567
