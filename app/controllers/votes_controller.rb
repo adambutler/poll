3bf699e8aed7a7c03e42f06d3e02989a81422f6b
Adam Butler <adam@littlebigmedia.co.uk>
2014-10-22 13:12:43 +0100

    Implement throttling

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,17 +1,18 @@
 class VotesController < ApplicationController
   before_filter :set_question, only: [:show]
+  before_filter :set_question_id, only: [:update]
+  before_filter :set_vote_id, only: [:update]
+  before_filter :throttle, only: [:update]
 
   def update
-    question_id = params[:vote][:question_id]
-    vote_id = cookies["vote_#{question_id}"]
-    vote = Vote.where({secret: vote_id}).first_or_initialize
+    vote = Vote.where({secret: @vote_id}).first_or_initialize
 
-    question = Question.where({secret: question_id}).first
+    question = Question.where({secret: @question_id}).first
     vote.secret = SecureRandom.urlsafe_base64(nil, false) unless vote.secret
     vote.question_id = question.id unless vote.question_id
     vote.option_id = Option.find(params[:vote][:option_id]).id
 
-    Pusher[question_id].trigger("vote", {})
+    Pusher[@question_id].trigger("vote", {})
 
     if vote.save!
       cookies.permanent["vote_#{question.secret}"] = vote.secret
@@ -26,6 +27,25 @@ class VotesController < ApplicationController
   def show
   end
 
+  def throttle
+    unless Vote.where({secret: @vote_id}).exists?
+      client_ip = env["REMOTE_ADDR"]
+      agent_hash = Digest::MD5.hexdigest(env["HTTP_USER_AGENT"])
+      key = "vote:#{client_ip}-#{agent_hash}"
+      count = REDIS.get(key)
+      unless count
+        REDIS.set(key, 0)
+        REDIS.expire(key, 10)
+      end
+
+      if count.to_i >= 1
+        render nothing: true, status: 429
+      else
+        REDIS.incr(key)
+      end
+    end
+  end
+
   private
 
   def set_question
@@ -34,5 +54,13 @@ class VotesController < ApplicationController
 
   def question_params
     params.require(:question).permit(:title)
+  end
+
+  def set_question_id
+    @question_id = params[:vote][:question_id]
+  end
+
+  def set_vote_id
+    @vote_id = cookies["vote_#{@question_id}"]
   end
 end
